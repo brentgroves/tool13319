62494c9d3cb3ce1b7191f6a9b597732cb5602c88
Brent Groves <brent.groves@gmail.com>
2021-02-11 16:13:09 -0500

    added/changed source

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -4,10 +4,10 @@ import { LogLevel } from "@azure/msal-browser";
 // Config object to be passed to Msal on creation
 export const msalConfig = {
   auth: {
-    clientId: "2c81136f-a43b-43c7-acd7-f1ebd851b839",
+    clientId: "9dcac2b4-f2f7-4570-ac75-d5f09eb346f5",
     authority:
       "https://login.microsoftonline.com/b4b87e8f-df64-41ff-9ba4-a4930ebc804b", // This is your tenant info
-    redirectUri: "https://eng",
+    redirectUri: "https://tooling",
   },
   system: {
     loggerOptions: {
